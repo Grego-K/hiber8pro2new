96eda18a49f1e82a4a5f8fa930b27642f30bc61c
Grigoris.K <grigoriskatsanakis@gmail.com>
2026-01-04 00:15:34 +0200

    feat:add JPQL for getting active teachers

--- src/main/java/gr/aueb/cf/schoolapp/App.java
+++ src/main/java/gr/aueb/cf/schoolapp/App.java
@@ -1,8 +1,26 @@
 package gr.aueb.cf.schoolapp;
 
+import gr.aueb.cf.schoolapp.model.Teacher;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
 
 public class App {
+
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("school8PU");
+    private static EntityManager em = emf.createEntityManager();
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        // Select active teachers - JPQL
+        TypedQuery<Teacher> query = em.createQuery("SELECT t FROM Teacher t WHERE active = true", Teacher.class);
+        List<Teacher> teachers = query.getResultList();
+        teachers.forEach(System.out::println);
+
+
+
     }
 }
