2395348ce6d3f7b3c277db626db3b981315237c2
Grigoris.K <grigoriskatsanakis@gmail.com>
2026-01-04 00:28:46 +0200

    feat:add more JPQL and criteria queries

--- src/main/java/gr/aueb/cf/schoolapp/App.java
+++ src/main/java/gr/aueb/cf/schoolapp/App.java
@@ -1,13 +1,12 @@
 package gr.aueb.cf.schoolapp;
 
+import gr.aueb.cf.schoolapp.model.Region;
 import gr.aueb.cf.schoolapp.model.Teacher;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import jakarta.persistence.TypedQuery;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.*;
 
 import java.util.List;
 
@@ -24,16 +23,44 @@ public class App {
 //        teachers.forEach(System.out::println);
 
 // Criteria API - Select active teachers
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<Teacher> query1 = cb.createQuery(Teacher.class);
-        Root<Teacher> teacher = query1.from(Teacher.class);
-        query1.select(teacher)
-                .where(
-                        cb.isTrue(teacher.get("active"))
-                );
+//        CriteriaBuilder cb = em.getCriteriaBuilder();
+//        CriteriaQuery<Teacher> query1 = cb.createQuery(Teacher.class);
+//        Root<Teacher> teacher = query1.from(Teacher.class);
+//        query1.select(teacher)
+//                .where(
+//                        cb.isTrue(teacher.get("active"))
+//                );
+//
+//        List<Teacher> teachers = em.createQuery(query1).getResultList();
+//        teachers.forEach(System.out::println);
 
-        List<Teacher> teachers = em.createQuery(query1).getResultList();
-        teachers.forEach(System.out::println);
+        // Select active teachers from "Αθήνα" - JPQL
+//        String sql = "SELECT t FROM Teacher t WHERE active = true AND t.region.title = :regionTitle";
+//        TypedQuery<Teacher> query = em.createQuery(sql, Teacher.class);
+//        query.setParameter("regionTitle", "Αθήνα");
+//        List<Teacher> activeTeachersInRegion = query.getResultList();
+//        activeTeachersInRegion.forEach(System.out::println);
+
+        // Select active teachers from "Αθήνα" - Criteria API
+//        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+//
+//        CriteriaQuery<Teacher> query = criteriaBuilder.createQuery(Teacher.class);
+//        Root<Teacher> teacher = query.from(Teacher.class);
+//        Join<Teacher, Region> region = teacher.join("region");
+//        ParameterExpression<String> regionTitle = criteriaBuilder.parameter(String.class);
+//
+//        query.select(teacher)
+//                .where(
+//                        criteriaBuilder.and(
+//                                criteriaBuilder.isTrue(teacher.get("active")),
+//                                criteriaBuilder.equal(region.get("title"), regionTitle)
+//                        )
+//                );
+//
+//        List<Teacher> activeTeachersInRegion = em.createQuery(query)
+//                .setParameter(regionTitle, "Αθήνα")
+//                .getResultList();
+//        activeTeachersInRegion.forEach(System.out::println);
 
     }
 }
