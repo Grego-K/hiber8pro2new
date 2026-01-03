8ffd8c097f49c404d3abb7c2ba9badac7641936c
Grigoris.K <grigoriskatsanakis@gmail.com>
2026-01-04 00:21:37 +0200

    feat:add criteria query for getting active teachers

--- src/main/java/gr/aueb/cf/schoolapp/App.java
+++ src/main/java/gr/aueb/cf/schoolapp/App.java
@@ -5,6 +5,9 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 
 import java.util.List;
 
@@ -16,11 +19,21 @@ public class App {
     public static void main(String[] args) {
 
         // Select active teachers - JPQL
-        TypedQuery<Teacher> query = em.createQuery("SELECT t FROM Teacher t WHERE active = true", Teacher.class);
-        List<Teacher> teachers = query.getResultList();
+//        TypedQuery<Teacher> query = em.createQuery("SELECT t FROM Teacher t WHERE active = true", Teacher.class);
+//        List<Teacher> teachers = query.getResultList();
+//        teachers.forEach(System.out::println);
+
+// Criteria API - Select active teachers
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Teacher> query1 = cb.createQuery(Teacher.class);
+        Root<Teacher> teacher = query1.from(Teacher.class);
+        query1.select(teacher)
+                .where(
+                        cb.isTrue(teacher.get("active"))
+                );
+
+        List<Teacher> teachers = em.createQuery(query1).getResultList();
         teachers.forEach(System.out::println);
-
-
 
     }
 }
